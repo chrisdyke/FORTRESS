95a7c53516cde918bab039ecdb30cad93486e3c1
dyke <fortresystem@gmail.com>
2022-06-17 22:16:36 +0100

    first git projet, test mode with dyke and joe

---
+++ events.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="en">
+
+<!-- index-sp-layout309:19-->
+<head>
+
+<!-- Meta Tags -->
+<meta name="viewport" content="width=device-width,initial-scale=1.0"/>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<meta name="description" content="LearnPress | Education & Courses HTML Template" />
+<meta name="keywords" content="academy, course, education, education html theme, #, learning," />
+
+
+<!-- Page Title -->
+<title>Chisado Music</title>
+
+<!-- Favicon and Touch Icons -->
+<link href="images/favicon.png" rel="shortcut icon" type="image/png">
+<!-- Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css">
+<link href="css/jquery-ui.min.css" rel="stylesheet" type="text/css">
+<link href="css/animate.css" rel="stylesheet" type="text/css">
+<link href="css/css-plugin-collections.css" rel="stylesheet"/>
+<!-- CSS | menuzord megamenu skins -->
+<link id="menuzord-menu-skins" href="css/menuzord-skins/menuzord-rounded-boxed.css" rel="stylesheet"/>
+<!-- CSS | Main style file -->
+<link href="css/style-main.css" rel="stylesheet" type="text/css">
+<!-- CSS | Preloader Styles -->
+<link href="css/preloader.css" rel="stylesheet" type="text/css">
+<!-- CSS | Custom Margin Padding Collection -->
+<link href="css/custom-bootstrap-margin-padding.css" rel="stylesheet" type="text/css">
+<!-- CSS | Responsive media queries -->
+<link href="css/responsive.css" rel="stylesheet" type="text/css">
+<!-- CSS | Style css. This is the file where you can place your own custom css code. Just uncomment it and use it. -->
+<!-- <link href="css/style.css" rel="stylesheet" type="text/css"> -->
+
+<!-- Revolution Slider 5.x CSS settings -->
+<link  href="js/revolution-slider/css/settings.css" rel="stylesheet" type="text/css"/>
+<link  href="js/revolution-slider/css/layers.css" rel="stylesheet" type="text/css"/>
+<link  href="js/revolution-slider/css/navigation.css" rel="stylesheet" type="text/css"/>
+
+<!-- CSS | Theme Color -->
+<link href="css/colors/theme-skin-color-set-1.css" rel="stylesheet" type="text/css">
+
+<!-- external javascripts -->
+<script src="js/jquery-2.2.4.min.js"></script>
+<script src="js/jquery-ui.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!-- JS | jquery plugin collection for this theme -->
+<script src="js/jquery-plugin-collection.js"></script>
+
+<!-- Revolution Slider 5.x SCRIPTS -->
+<script src="js/revolution-slider/js/jquery.themepunch.tools.min.js"></script>
+<script src="js/revolution-slider/js/jquery.themepunch.revolution.min.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+<body class="">
+<div id="wrapper" class="clearfix">
+  <!-- preloader -->
+  <div id="preloader">
+    <div id="spinner">
+      <img alt="" src="images/preloaders/5.gif">
+    </div>
+    <div id="disable-preloader" class="btn btn-default btn-sm">Disable Preloader</div>
+  </div>
+  <?php include('header.php') ; ?>
+  <br/>
+   <!-- Section: home -->
+    <section id="home" class="bg-lightest fullscreen">
+      <div class="display-table text-center">
+        <div class="display-table-cell">
+          <div class="container pt-0 pb-0"><div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <h1 class="text-theme-colored font-weight-100 font-64">We Are Coming Soon</h1>                
+                
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div id="countdown17" class="ClassyCountdownDemo"></div>
+                                    
+                    <!-- jQuery Classy Countdown Scripts and CSS -->
+                    <link href="js/classycountdown/css/jquery.classycountdown.css" rel="stylesheet" type="text/css">
+                    <script src="js/classycountdown/js/jquery.knob.js"></script> 
+                    <script src="js/classycountdown/js/jquery.throttle.js"></script> 
+                    <script src="js/classycountdown/js/jquery.classycountdown.js"></script> 
+
+                    <!-- Classy Countdown Script -->
+                    <script type="text/javascript">
+                      $(document).ready(function() {
+                        $('#countdown17').ClassyCountdown({
+                            theme: "flat-colors-very-wide",
+                            end: $.now() + 10000
+                        });
+                      });
+                    </script>
+                  </div>
+                </div>
+              </div>
+              
+              <div class="col-md-8 col-md-offset-2">
+                <form id="mailchimp-subscription-form" class="newsletter-form mt-0 mb-30 maxwidth500">
+                  <label for="mce-EMAIL"></label>
+                  <div class="input-group">
+                    <input type="email" id="mce-EMAIL" data-height="45px" class="form-control input-lg" placeholder="Your Email" name="EMAIL" value="">
+                    <span class="input-group-btn">
+                      <button type="submit" class="btn btn-colored btn-theme-colored btn-xs m-0" data-height="45px">Subscribe</button>
+                    </span>
+                  </div>
+                </form>
+
+                
+
+                <p class="font-14">This Page will soon be here fully</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <br/>
+    <?php include('footer.php') ; ?>
+<a class="scrollToTop" href="#"><i class="fa fa-angle-up"></i></a>
+</div>
+<!-- end wrapper --> 
+
+<!-- Footer Scripts --> 
+<!-- JS | Custom script for all pages --> 
+<script src="js/custom.js"></script>
+
+<!-- SLIDER REVOLUTION 5.0 EXTENSIONS  
+      (Load Extensions only on Local File Systems ! 
+       The following part can be removed on Server for On Demand Loading) --> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.actions.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.carousel.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.kenburn.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.layeranimation.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.migration.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.navigation.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.parallax.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.slideanims.min.js"></script> 
+<script type="text/javascript" src="js/revolution-slider/js/extensions/revolution.extension.video.min.js"></script>
+</body>
+
+<!-- index-sp-layout309:19-->
+</html>
